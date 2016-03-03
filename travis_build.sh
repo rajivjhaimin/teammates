322109ca62fc6715a4a1a382b0e44fd529f1899e
Ashray Jain <ashrayj11@gmail.com>
2016-03-03 11:41:07 +0800

    add travis_build.sh

---
+++ travis_build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+./gradlew &
+minutes=0
+limit=60
+while kill -0 $! >/dev/null 2>&1; do
+    echo -n -e " \b" # never leave evidences!
+    if [ $minutes == $limit ]; then
+        break;
+    fi
+    minutes=$((minutes+1))
+    sleep 60
+done
+
